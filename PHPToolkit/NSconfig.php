195af93744fe75f99f30db591757951f7b8b7d37
Ryan McCaw <ryan@tribehr.com>
2014-01-16 11:13:17 -0500

    So because the $nshost variable was a global the SoapClient would try to find domains in getNetSuiteHosts() with the last $nshost used. After you push a lead on staging that gets set to webservices.sandbox.netsuite.com and then we try to get the hosts with that url but the account id we are using to look up the hosts does not exist on the sandbox environment.

--- PHPToolkit/NSconfig.php
+++ PHPToolkit/NSconfig.php
@@ -1,7 +1,6 @@
 <?php
 
 // Mark them in global context so that NSPHPClient can always find them.
-global $nsendpoint, $nshost;
+global $nsendpoint;
 
 $nsendpoint = "2012_2";
-$nshost = "https://webservices.netsuite.com";
